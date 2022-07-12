31e958f20caabb926e1ef6deb2b6bc429868b811
tetta-pdnt <mr.ttt2001@gmail.com>
2022-07-12 10:58:35 +0900

    adapt vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <!-- <HelloWorld/> -->
+      <HelloWorld v-for="p_q in p_qs" :key="p_q" :param="p_q.param" :query="p_q.query" v-on:r_inf="test($event)"/>
+  <!-- <p>{{book_inf}}</p> -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    p_qs:[{param:'title',query:'テスト'},
+            {param:'title',query:'太陽'}]
+  }),
+  methods: {
+    test(ev){
+      this.p_qs.push({param:'title',query:ev['title']})
+      this.p_qs.push({param:'author',query:ev['author']})
+      this.p_qs.push({param:'publisherName',query:ev['publisher']})
+    }
+  }
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,180 @@
+<!--<template>
+  <v-hover v-slot:default="{ hover }">
+    <v-card
+      :elevation="hover ? 12 : 2"
+
+      class="mx-auto"
+      max-width="344"
+    >
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="text-h5 mb-1">
+              Headline 5
+            </v-list-item-title>
+            <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar
+            tile
+            size="80"
+            color="grey"
+          ></v-list-item-avatar>
+        </v-list-item>
+
+        <v-card-actions>
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            Button
+          </v-btn>
+        </v-card-actions>
+
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>-->
+
+
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <v-btn elevation="2" @click="get()">{{param}}:{{query}}</v-btn><br>
+      <v-hover v-slot:default="{ hover }" v-if="rakuten_book.length!==0">
+    <v-card
+      :elevation="hover ? 12 : 2"
+      class="mx-auto"
+      max-width="300"
+    >
+    <v-img
+      :src='rakuten_book.largeImageUrl'
+      max-width="150">
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-row justify="end">
+            <v-card-title class="text-h5 ml-1 text-end">
+              {{rakuten_book.title}}
+            </v-card-title>
+            </v-row>
+            <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          </v-list-item-content>
+
+
+        </v-list-item>
+
+        <v-card-actions>
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            Button
+          </v-btn>
+        </v-card-actions>
+      </v-img>
+    </v-card>
+  </v-hover>   
+    <p></p>
+    <p>{{rakuten_book.author}}</p>
+    <p>{{rakuten_book.publisherName}}</p>
+    <p>{{rakuten_book.itemCaption}}</p>
+    <!-- <p>{{dataa}}</p> -->
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  props: ['param','query'],
+  data: () => ({
+      url: 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?applicationId=',
+      appid: '1036259511343410249',
+      rakuten_book: [],
+  }),
+  methods: {
+      fetchSample: async function(){
+      // 非同期処理を記述
+      let self = this
+      await axios.get(this.url + this.appid+
+                      '&booksGenreId=001004008'+
+                      '&'+self.param+'='+self.query)
+        .then(function (response) {
+          self.rakuten_book=response.data.Items[1].Item
+        })
+        .catch((error) => {
+          this.errorMsg = 'Error! Could not reach the API. ' + error
+          console.log(this.errorMsg)
+        })
+      return this.rakuten_book
+    },
+    get: async function(){
+      // this.fetchSample()の実行が完了するまで待機
+      let result = await this.fetchSample()
+      const r_inf = {'title':result.title,
+                    'author':result.author,
+                    'publisher':result.publisherName,}
+      this.$emit('r_inf',r_inf)
+    },
   }
 }
 </script>
@@ -56,3 +196,7 @@ a {
   color: #42b983;
 }
 </style>
+
+
+
+ 
